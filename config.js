819ab7f615216838d1ab1695ae3fa6eab29b3061
ameer <faraj.ameer@gmail.com>
2016-03-18 11:33:16 +0200

    Updated config file.

--- config.js
+++ config.js
@@ -3,6 +3,11 @@
 module.exports = {
     serviceName: 'test',
     serviceGroup: 'Test Group',
+    servicePort: 5566,
+    extKeyRequired: false,
+    awareness: false,
+    requestTimeout: 30,
+    requestTimeoutRenewal: 5,
     type: 'service',
     multi: null,
     main: 'index.js'
