1fd4106b913903a65a5d9634ddc2ff546c5a16bf
ameer <faraj.ameer@gmail.com>
2016-03-21 13:56:32 +0200

    Added edge case to config file

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var test1 = require('../someFile');
+var test2 = require('../anotherFile'),
+    test3 = require('../lastFile');
+
 module.exports = {
     serviceName: 'test',
     serviceGroup: 'Test Group',
